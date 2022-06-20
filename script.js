78552873da9595fe952f378ec01860eca7a6d010
JatinKumar <jatinkumar6489@gmail.com>
2022-06-20 14:58:08 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,152 @@
+const stars0 = document.querySelectorAll('.s0');
+
+stars0.forEach((s0, i)=>{
+    s0.onclick = function(){
+        let currlevel0 = i + 1;
+
+        stars0.forEach((s0, j)=>{
+            if(currlevel0 >= j+1){
+                s0.innerHTML = '&#9733';
+            }
+            else{
+                s0.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars1 = document.querySelectorAll('.s1');
+
+stars1.forEach((s1, i)=>{
+    s1.onclick = function(){
+        let currlevel1 = i + 1;
+
+        stars1.forEach((s1, j)=>{
+            if(currlevel1 >= j+1){
+                s1.innerHTML = '&#9733';
+            }
+            else{
+                s1.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars2 = document.querySelectorAll('.s2');
+
+stars2.forEach((s2, i)=>{
+    s2.onclick = function(){
+        let currlevel2 = i + 1;
+
+        stars2.forEach((s2, j)=>{
+            if(currlevel2 >= j+1){
+                s2.innerHTML = '&#9733';
+            }
+            else{
+                s2.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars3 = document.querySelectorAll('.s3');
+
+stars3.forEach((s3, i)=>{
+    s3.onclick = function(){
+        let currlevel3 = i + 1;
+
+        stars3.forEach((s3, j)=>{
+            if(currlevel3 >= j+1){
+                s3.innerHTML = '&#9733';
+            }
+            else{
+                s3.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars4 = document.querySelectorAll('.s4');
+
+stars4.forEach((s4, i)=>{
+    s4.onclick = function(){
+        let currlevel4 = i + 1;
+
+        stars4.forEach((s4, j)=>{
+            if(currlevel4 >= j+1){
+                s4.innerHTML = '&#9733';
+            }
+            else{
+                s4.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars5 = document.querySelectorAll('.s5');
+
+stars5.forEach((s5, i)=>{
+    s5.onclick = function(){
+        let currlevel5 = i + 1;
+
+        stars5.forEach((s5, j)=>{
+            if(currlevel5 >= j+1){
+                s5.innerHTML = '&#9733';
+            }
+            else{
+                s5.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars6 = document.querySelectorAll('.s6');
+
+stars6.forEach((s6, i)=>{
+    s6.onclick = function(){
+        let currlevel6 = i + 1;
+
+        stars6.forEach((s6, j)=>{
+            if(currlevel6 >= j+1){
+                s6.innerHTML = '&#9733';
+            }
+            else{
+                s6.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars7 = document.querySelectorAll('.s7');
+
+stars7.forEach((s7, i)=>{
+    s7.onclick = function(){
+        let currlevel7 = i + 1;
+
+        stars7.forEach((s7, j)=>{
+            if(currlevel7 >= j+1){
+                s7.innerHTML = '&#9733';
+            }
+            else{
+                s7.innerHTML = '&#9734';
+            }
+        })
+    }
+})
+
+const stars8 = document.querySelectorAll('.s8');
+
+stars8.forEach((s8, i)=>{
+    s8.onclick = function(){
+        let currlevel8 = i + 1;
+
+        stars8.forEach((s8, j)=>{
+            if(currlevel8 >= j+1){
+                s8.innerHTML = '&#9733';
+            }
+            else{
+                s8.innerHTML = '&#9734';
+            }
+        })
+    }
+})
